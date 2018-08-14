1f70345c1fb08fb1e3829566388ee7ff13071b36
Mohsen Khadem <m.khadem@ucl.ac.uk>
2018-08-14 18:18:34 +0100

    Update, calculates shape using analytical methods

--- Tube_Code.m
+++ Tube_Code.m
@@ -8,15 +8,15 @@ clc
 
 param  % load tube parameters inside param.m file
     
-tic
+
 l=0.01*[45 30 20];   % length of tubes 
 B=0.01*[-14 -10 -5];  % length of tubes before template
 l_k=0.01*[10 10 15]; % length of curved part of tubes
 
 %initial angles
-alpha_1=0;
-alpha_2=0;
-alpha_3=0;
+alpha_1=pi/2;
+alpha_2=pi/2;
+alpha_3=pi/2;
 alpha=[0 alpha_2-alpha_1 alpha_3-alpha_1];
 
 % segmenting tubes  
@@ -70,7 +70,7 @@ Length=[]; U_x=[]; U_y=[]; U_z=[]; Alpha=[];   % solved length, curvatures, and 
 
 for seg=1:length(S)
     
-s_span = [span(seg) span(seg+1)-0.0001];
+s_span =span(seg):0.0001:span(seg+1)-0.0001;
 y_0=zeros(2*n,1);
 y_0(n+1:2*n)=alpha;
 
@@ -131,6 +131,8 @@ R0=[cos(alpha_1) sin(alpha_1) 0; -sin(alpha_1) cos(alpha_1) 0; 0 0 1]; % why sho
 
 y_0=[0; 0 ;0 ;reshape(R0,[9,1])];
 [s,y] = ode45(@(s,y) ode2(s,y,U_x,U_y,U_z,Length), [0 Length(end)], y_0);
+R=[y(end,4) y(end,5) y(end,6);y(end,7) y(end,8) y(end,9);y(end,10) y(end,11) y(end,12)]
+
 %y(1) to y(3) are x , y, and z position of point materials
 toc
 r1=[y(:,1),y(:,2),y(:,3)];
@@ -142,8 +144,55 @@ r3=[y(1:tube3_end,1),y(1:tube3_end,2),y(1:tube3_end,3)];
 figure(1);
 plot3(r1(:,1),r1(:,2),r1(:,3),'b','LineWidth',2)
 hold on
-plot3(r2(:,1),r2(:,2),r2(:,3),'r','LineWidth',4)
-plot3(r3(:,1),r3(:,2),r3(:,3),'g','LineWidth',6)
+%plot3(r2(:,1),r2(:,2),r2(:,3),'r','LineWidth',4)
+%plot3(r3(:,1),r3(:,2),r3(:,3),'g','LineWidth',6)
 xlabel('X [mm]'); ylabel('Y [m]'); zlabel('Z [m]')
 grid on
 axis equal
+
+%%  Calculating shape analtycally
+int_ux=zeros(1,length(Length)); int_uy=s;zeros(1,length(Length)); int_uz=zeros(1,length(Length));
+R=zeros(3,3*(length(Length))); R(:,1:3)=R0;
+int_ux(1)=U_x(1,1); int_uy(1)=U_y(1,1); int_uz(1)=U_z(1,1);
+
+
+int_ux=cumtrapz(Length,U_x(:,1));
+int_uy=cumtrapz(Length,U_y(:,1));
+int_uz=cumtrapz(Length,U_z(:,1));
+
+for i=1:length(Length)-1
+    
+%int_ux(i+1)=trapz(Length(1:i+1),-U_x(1:i+1,1));
+%int_uy(i+1)=trapz(Length(1:i+1),-U_y(1:i+1,1));
+%int_uz(i+1)=trapz(Length(1:i+1),-U_z(1:i+1,1));
+
+RR=R0'*expm ([ 0 -int_uz(i) int_uy(i); int_uz(i) 0 -int_ux(i); -int_uy(i) int_ux(i) 0]);  
+% transpose of R0 is used because R0 is in body frame (line 130)
+R(:,3*i+1:3*(i+1))=RR;
+
+end
+
+r=zeros(3,length(Length)); rr=r;
+
+r(1,:)=cumtrapz(Length, R(1,3:3:end) );
+r(2,:)=cumtrapz(Length, R(2,3:3:end) );
+r(3,:)=cumtrapz(Length, R(3,3:3:end) );
+
+
+
+u1=int_ux; u2=int_uy; u3=int_uz;
+x=sqrt(u1.^2+u2.^2+u3.^2);
+rr1= sin(alpha_1).*((u1.*sin(x))./x + (u2.*u3.*(cos(x) - 1))./x.^2) + cos(alpha_1).*((u2.*sin(x))./x - (u1.*u3.*(cos(x) - 1))./x.^2);
+rr2= sin(alpha_1).*((u2.*sin(x))./x - (u1.*u3.*(cos(x) - 1))./x.^2) - cos(alpha_1).*((u1.*sin(x))./x + (u2.*u3.*(cos(x) - 1))./x.^2);
+rr3= ((u1.^2 + u2.^2).*(cos(x) - 1))./x.^2 + 1;
+rr1(1)=0; rr2(1)=0; rr3(1)=0;                                                                              
+
+                        
+rr(1,:)=cumtrapz(Length, rr1');
+rr(2,:)=cumtrapz(Length, rr2');
+rr(3,:)=cumtrapz(Length, rr3');
+
+                         
+plot3(r(1,:),r(2,:),r(3,:),'--r','LineWidth',2)
+hold on
+plot3(rr(1,:),rr(2,:),rr(3,:),'--g','LineWidth',2)
